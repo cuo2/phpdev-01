8be04fc9cfa79c972a8b13dc6235bcf6a049ecee
cuo2 <avpimankin19@gmail.com>
2022-02-03 23:37:56 +0700

    INIT initial commit

--- index.php
+++ index.php
@@ -1,6 +1,19 @@
+<?php
+
+include 'node.php';
+
+interface Car { 
+    public function setModel($name);  
+    public function getModel();
+  }
+  
+?>
+
+
 <html>
     <body>
-        <h1><?php echo "Hello world"; ?></h1>
-        I'm<?php echo ['name'] ?? 'noname'; ?>
+    <h1><?php echo "Hello world!"; ?></h1>
+        <p>I'm <?php echo $_REQUEST['name'] ?? 'Noname'; ?></p>
+        <pre><?php echo $car -> getModel(); ?></pre>
     </body>
 </html>
---
+++ node.php
@@ -0,0 +1,18 @@
+<?php
+
+class miniCar implements Car {
+  private $model; 
+   
+  public function setModel($name) { 
+    $this -> model = $name; 
+  }  
+  public function getModel() {
+    return $this -> model; 
+  }
+}
+
+$car = new miniCar();
+$car -> setModel('Mercedes Benz');
+
+ 
+?>
